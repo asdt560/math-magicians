3785dca19af3bca161275693025b0b0cf691f53d
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-24 19:15:44 -0300

    Create Calculator class

---
+++ src/components/Calculator.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// eslint-disable-next-line
+class Calculator extends React.Component {
+  render() {
+    return (
+      <main>
+        <div className="column top">0</div>
+        <div className="column">1</div>
+        <div className="column">2</div>
+        <div className="column">3</div>
+        <div className="column">2</div>
+        <div className="column">3</div>
+      </main>
+    );
+  }
+}
+
+export default Calculator;
