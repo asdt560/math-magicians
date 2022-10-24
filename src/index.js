00d1203e8b74b01a56493a31736dfdc162e230e2
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-10-24 20:41:26 -0300

    Build basic calculator class

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Calculator from './components/Calculator';
 import reportWebVitals from './reportWebVitals';
 
+const Title = () => {
+  useEffect(() => {
+    document.title = 'Calculator';
+  });
+  return (
+    <section hidden><div>Lets do some Math</div></section>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Title />
     <Calculator />
   </React.StrictMode>,
 );
+
+Title();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
